b1fd5399f1e5c0d49d58868adbc47269d84c7ceb
JonSolow <jssolow+gh@gmail.com>
2019-07-10 20:52:05 -0400

    Initialize repo

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A model to predict when a TV show will end at any ppoint in the series.',
+    author='Jon Solow',
+    #license='',
+)
